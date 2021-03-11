0e9ea5676ab966068ab81c1514dc01b02cfe5f92
DarkNight <truonguit2010@gmail.com>
2021-03-11 16:12:20 +0700

    - Done the tutorial.

--- batch-processing/src/main/java/com/example/batchprocessing/BatchProcessingApplication.java
+++ batch-processing/src/main/java/com/example/batchprocessing/BatchProcessingApplication.java
@@ -3,11 +3,13 @@ package com.example.batchprocessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// https://spring.io/guides/gs/batch-processing/
 @SpringBootApplication
 public class BatchProcessingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BatchProcessingApplication.class, args);
+//		SpringApplication.run(BatchProcessingApplication.class, args);
+		System.exit(SpringApplication.exit(SpringApplication.run(BatchProcessingApplication.class, args)));
 	}
 
 }
